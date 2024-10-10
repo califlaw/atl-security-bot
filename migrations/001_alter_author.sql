919fbc87dc9abe924420be8a74cbc3e7a3138767
@kdelinxd <kdelinx+1@gmail.com>
2024-10-10 17:43:14 +0600

    hotfix migration

--- migrations/001_alter_author.sql
+++ migrations/001_alter_author.sql
@@ -2,7 +2,7 @@
 alter table author add column if not exists exp int not null default 0;
 alter table author add column if not exists tg_username varchar(80) not null default '';
 alter table author add column if not exists email varchar(80) null;
-alter table author alter column tg_user_id type biging using tg_user_id::biging;
+alter table author alter column tg_user_id type bigint using tg_user_id::bigint;
 alter table author alter id set default gen_random_uuid();
 
 
