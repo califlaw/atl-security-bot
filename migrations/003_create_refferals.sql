9040e0b41cdf3e8efc62f741c418fcbdf77d0115
@kdelinxd <kdelinx+1@gmail.com>
2024-10-18 13:38:26 +0600

    hotfix default value for `referrers`

--- migrations/003_create_refferals.sql
+++ migrations/003_create_refferals.sql
@@ -3,6 +3,6 @@ create table if not exists referrals
     id            serial primary key,
     author_id     uuid not null references author deferrable initially deferred unique,
     referrer_code varchar(255) unique,
-    referrers     bigint array,
+    referrers     bigint array default '{}',
     created_at    timestamptz default now()
 );
